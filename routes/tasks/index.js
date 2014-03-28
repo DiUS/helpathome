b8c9c7bb43110c973d9ce420532a351949dcec56
StephenSmithwick <Stephen.Smithwick@gmail.com>
2014-03-28 11:25:52 +1100

    updating json structure with task id and reference to put against

--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -1,8 +1,9 @@
 var id = 0;
-exports.index = function(req, res){
+exports.index = function(req, res) {
   res.json({ 
-    task_id: id++,
-    task: 'function(input) { alert(input); }',
-    data: 'Hello from help@home'
+    task_id: id,
+    result_url: "//tasks/" + id + "/result",
+    task: 'function(input) { input + 1 }',
+    data: id
   });
 };

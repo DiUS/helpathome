88ed215eaab8d07987761d2a36682d82e1f5f81c
StephenSmithwick <Stephen.Smithwick@gmail.com>
2014-03-28 12:05:21 +1100

    moving tasks to tasks.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 
 var express = require('express');
 var routes = require('./routes');
-var tasks = require('./routes/tasks');
+var tasks = require('./routes/tasks.js');
+var results = require('./routes/results.js');
 
 var app = module.exports = express.createServer();
 
@@ -36,6 +37,7 @@ app.get('/tasks', tasks.index);
 app.get('/tasks/next', tasks.next);
 app.get('/tasks/:id', tasks.show);
 
+//Results
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);

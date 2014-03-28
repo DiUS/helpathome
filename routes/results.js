bd881c6c04ce19932e44cbc434b3cef01880bf6e
StephenSmithwick <Stephen.Smithwick@gmail.com>
2014-03-28 12:21:10 +1100

    Adding results to a task is now possible

--- routes/results.js
+++ routes/results.js
@@ -1,1 +1,20 @@
-"TODO"
+var results = []
+
+exports.set = function(req, res) {
+  var task_id = req.params.task_id;
+  var data = req.body;
+
+  res.json(store(task_id, data));
+};
+
+exports.show = function(req, res) {
+  var task_id = req.params.task_id;
+  var result = results[task_id] || {};
+
+  res.json(result);
+};
+
+function store(task_id, data) {
+  results[task_id] = data;
+  return data;
+}
